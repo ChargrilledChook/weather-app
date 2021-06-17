e35d71262273ef6c9dda663f0669a0f41c8808b7
chook <dylanw212@gmail.com>
2021-06-17 13:06:21 +1000

    add favicon to site

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-const key = "";
+const key = "bcd9efe90d4b0bbc5b7e653dc9004c70";
 
-const button = document.querySelector('button')
-button.addEventListener('click', displayWeather)
+const button = document.querySelector("button");
+button.addEventListener("click", displayWeather);
 
-
-const wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector(".wrapper");
 
 async function fetchData() {
-  const searchTerm = document.querySelector('input').value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${key}&units=metric`
-  console.log(url)
-  const response = await fetch(url, {mode: 'cors'})
+  const searchTerm = document.querySelector("input").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${key}&units=metric`;
+  console.log(url);
+  const response = await fetch(url, { mode: "cors" });
   const data = await response.json();
-  console.table(data)
+  console.table(data);
   return data;
 }
 
-async function displayWeather(){
+async function displayWeather() {
   const data = await fetchData();
-  console.log(data)
+  console.log(data);
   const weather = createContainer(data);
-  wrapper.append(weather)
+  wrapper.append(weather);
 }
 
-
 function createContainer(data) {
-  const container = document.createElement('div');
-  container.classList.add('result')
-  const header = document.createElement('h2');
+  const container = document.createElement("div");
+  container.classList.add("result");
+  const header = document.createElement("h2");
   header.textContent = `${data.name}, ${data.sys.country}`;
 
-  const tempContainer = document.createElement('div');
+  const tempContainer = document.createElement("div");
   const temp = data.main.temp;
-  tempContainer.textContent = `Current temperature in ${data.name} is ${temp} degrees celcius`
+  tempContainer.textContent = `Current temperature in ${data.name} is ${temp} degrees celcius`;
 
-  container.append(header, tempContainer)
+  container.append(header, tempContainer);
 
   return container;
 }
